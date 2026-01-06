65e1ab033911cf0a6bd2b3cba5505a681e68f564
maen thaher <maentcefe@gmail.com>
2026-01-06 07:39:07 +0200

    add const like activeTab,error,success at index.tsx for login signup

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,15 @@ import { useAuth } from "../context/AuthContext";
 
 
 
-
 export default function AuthenticationScreen(){
 
 const {user,login,signup,error,authError,isLoading}=useAuth();
 
 
-
+const [activeTab, setActiveTab] = useState<"login" | "signup">("login");
+  const [showForgotPassword, setShowForgotPassword] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
 
 
