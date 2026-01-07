97f188cde9ef32cf435cbf2318ff7790854d3222
maen thaher <maentcefe@gmail.com>
2026-01-07 07:09:29 +0200

    add footer

--- app/About.tsx
+++ app/About.tsx
@@ -1,12 +1,17 @@
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 
-
 export default function About(){
 
 return(<ScrollView style={styles.container}>
 
 <View style={styles.header}>
 <Text style={styles.headerText}>CircuitAi</Text>
+</View>
+
+
+
+<View style={styles.footer}>
+<Text style={styles.footerText}>© CircuitAI 2026</Text>
 </View>
 
 </ScrollView>);
@@ -19,6 +24,9 @@ const styles = StyleSheet.create
 ({
   container:{flex:1,backgroundColor:"blue"},
   header:{padding:20,backgroundColor:"red",alignItems:"center"},
-  headerText:{fontSize:24,fontWeight:"bold",color:"green"}
+  headerText:{fontSize:24,fontWeight:"bold",color:"green"},
+  footer:{padding:20,alignItems:"center",backgroundColor:"yellow"},
+  footerText:{fontSize:14,color:"orange"}
 });
+
 
