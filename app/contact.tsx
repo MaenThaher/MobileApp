0d6a171ae06b08d8c707bc993e6f06e300fa0129
maen thaher <maentcefe@gmail.com>
2026-01-09 09:03:22 +0200

    add succesMessage and SuccessText

--- app/contact.tsx
+++ app/contact.tsx
@@ -2,9 +2,11 @@ import { Text } from "@react-navigation/elements";
 import { useState } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
 
+
 export default function Contact(){
 const [submitted,setSubmitted]=useState(false);
 const [role,setRole]=useState<string | null>(null);
+
 
 
 return(<ScrollView contentContainerStyle={styles.container}>
@@ -15,10 +17,27 @@ return(<ScrollView contentContainerStyle={styles.container}>
     Contact us
 </Text>
 
-        <Text style={styles.introText}>
+
+ <Text style={styles.introText}>
             Questions, feedback, or interested in using CircuitAI in your course?
             Send us a message.
-        </Text>
+ </Text>
+ 
+ { submitted ? 
+ (
+ <View style={styles.successMessage}>
+
+<Text style={styles.successText}>
+Thanks! we well get back to you soon.
+</Text>
+
+ </View>
+ ):(
+<></>
+ )
+ 
+
+ }
 
 
 
@@ -54,6 +73,8 @@ container: {
     color:"#aaa",
     marginBottom:24,
   },
+  successMessage:{padding:20,backgroundColor:"rgba(79,70,229,0.15)"},
+  successText:{color:"#4f46e5",fontWeight:"600",textAlign:"center"},
 
 
 })
