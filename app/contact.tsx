0942224714c0de8311455699fa35a360f64a507a
maen thaher <maentcefe@gmail.com>
2026-01-09 08:47:45 +0200

    add page title and Quesitons feed back Text

--- app/contact.tsx
+++ app/contact.tsx
@@ -1,14 +1,21 @@
+import { Text } from "@react-navigation/elements";
 import { ScrollView, StyleSheet, View } from "react-native";
-
 
 
 export default function Contact(){
 
 return(<ScrollView contentContainerStyle={styles.container}>
 <View style={styles.card}>
-<View>
+<View style={styles.formColumn}>
 
+<Text style={styles.pageTitle}>
+    Contact us
+</Text>
 
+        <Text style={styles.introText}>
+            Questions, feedback, or interested in using CircuitAI in your course?
+            Send us a message.
+        </Text>
 
 </View>
  
@@ -26,9 +33,21 @@ container: {
     flexGrow: 1,
     justifyContent: "center",
   },
+  formColumn:{
+  padding:24,
+  },
+  pageTitle:{
+    fontSize:32,
+    fontWeight:"700",color:"#fff",
+    marginBottom:12,
+  },
   card:{
     backgroundColor:"black",borderRadius:16,
     padding:24,
+  },
+  introText:{fontSize:16,
+    color:"#aaa",
+    marginBottom:24,
   },
 
 
