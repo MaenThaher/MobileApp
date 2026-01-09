ac974293b1ef574e1d267df82232f15c00dabb26
maen thaher <maentcefe@gmail.com>
2026-01-09 08:35:43 +0200

    add container and card styles

--- app/contact.tsx
+++ app/contact.tsx
@@ -1,9 +1,35 @@
-import { ScrollView } from "react-native"
+import { ScrollView, StyleSheet, View } from "react-native";
+
+
 
 export default function Contact(){
 
-return(<ScrollView>
-    
+return(<ScrollView contentContainerStyle={styles.container}>
+<View style={styles.card}>
+<View>
+
+
+
+</View>
+ 
+</View>
+
 </ScrollView>)
 
 }
+
+
+const styles = StyleSheet.create({
+container: {
+    padding: 20,
+    backgroundColor: "red",
+    flexGrow: 1,
+    justifyContent: "center",
+  },
+  card:{
+    backgroundColor:"black",borderRadius:16,
+    padding:24,
+  },
+
+
+})
