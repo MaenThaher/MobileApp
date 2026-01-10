880909cb228f16910644665250727d7ff9e855d6
maen thaher <maentcefe@gmail.com>
2026-01-10 12:44:02 +0200

    Admin Dahs board with Styles

--- app/admin.tsx
+++ app/admin.tsx
@@ -1,10 +1,20 @@
-import { ScrollView } from "react-native"
-
+import { Text } from "@react-navigation/elements";
+import { ScrollView, StyleSheet } from "react-native";
 
 export default function AdminDashboard(){
-  
-return(<ScrollView>
+
+return(<ScrollView style = {styles.page}>
+
+<Text style={styles.title}>Admin Dashboard</Text>
+
 
 </ScrollView>)
 
 }
+
+const styles = StyleSheet.create({
+page:{padding:20,backgroundColor:"#0f0f0f",flexGrow:1},
+title:{fontSize:30,fontWeight:"700",color:"#fff",marginBottom:8},
+
+});
+
