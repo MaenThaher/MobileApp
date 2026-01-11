db76b4d3e2f6462704ab57b1098489e7776e5ca6
maen thaher <maentcefe@gmail.com>
2026-01-11 20:42:24 +0200

    Crete and setIsCreateModalOpen useState

--- app/students.tsx
+++ app/students.tsx
@@ -1,11 +1,12 @@
 import { Feather } from "@expo/vector-icons";
 import { Text } from "@react-navigation/elements";
 import { useState } from "react";
-import { StyleSheet, TextInput } from "react-native";
+import { StyleSheet, TextInput, TouchableOpacity } from "react-native";
 import { View } from "react-native-reanimated/lib/typescript/Animated";
 
 export default function AdminStudents(){
 const [search,setSearch]=useState("");
+const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
 
     return (<View style={styles.container}>
     <View style={styles.header}>
@@ -14,6 +15,7 @@ const [search,setSearch]=useState("");
      
     <View style={styles.searchBox}>
  <Feather name="search" size={18} color="#666"/>
+   
     <TextInput 
     placeholder="Search students..."
     value={search}
@@ -23,6 +25,11 @@ const [search,setSearch]=useState("");
     
     </View>
     
+    <TouchableOpacity style={styles.createBtn} 
+    onPress={()=> setIsCreateModalOpen(true)}
+    >
+<Text style={styles.createText}>Create</Text>
+    </TouchableOpacity>
 
     </View>
      
@@ -39,5 +46,7 @@ title:{fontSize:24,fontWeight:"bold",marginBottom:12,},
 header:{marginBottom:16,},
 searchBox:{flexDirection:"row",alignItems:"center",borderWidth:1,borderColor:"#ddd",borderRadius:8,paddingHorizontal:10,marginBottom:10,},
 input:{flex:1,padding:8},
+createBtn:{backgroundColor:"#2563eb",padding:12,borderRadius:8,alignItems:"center"},
+createText:{color:"white",fontWeight:"600",},
 
 })
