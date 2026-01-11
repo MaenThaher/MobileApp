9925dd4db45e9649b33b9b91451b60396d88c37f
maen thaher <maentcefe@gmail.com>
2026-01-11 20:34:29 +0200

    useState for Search and add TextInput for search Student

--- app/students.tsx
+++ app/students.tsx
@@ -1,16 +1,43 @@
-import { StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { Text } from "@react-navigation/elements";
+import { useState } from "react";
+import { StyleSheet, TextInput } from "react-native";
 import { View } from "react-native-reanimated/lib/typescript/Animated";
 
 export default function AdminStudents(){
+const [search,setSearch]=useState("");
 
     return (<View style={styles.container}>
+    <View style={styles.header}>
+    
+    <Text style={styles.title}>Students</Text>     
+     
+    <View style={styles.searchBox}>
+ <Feather name="search" size={18} color="#666"/>
+    <TextInput 
+    placeholder="Search students..."
+    value={search}
+    onChangeText={setSearch}
+    style={styles.input}
+    />
+    
+    </View>
+    
 
+    </View>
+     
 
+  
     </View>)
 
 }
 
 
 const styles = StyleSheet.create({
-container:{flex:1,padding:16,}
+container:{flex:1,padding:16,},
+title:{fontSize:24,fontWeight:"bold",marginBottom:12,},
+header:{marginBottom:16,},
+searchBox:{flexDirection:"row",alignItems:"center",borderWidth:1,borderColor:"#ddd",borderRadius:8,paddingHorizontal:10,marginBottom:10,},
+input:{flex:1,padding:8},
+
 })
