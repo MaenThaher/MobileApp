7fab1c1a75fdc51f885b173120ff3b546be3af2d
ahmedjk34 <ahmedtaher212005@gmail.com>
2026-01-12 21:04:20 +0200

    Refactor InstructorTabsLayout to include tab icons and active tint color. Enhance user experience with Ionicons for better visual representation of tabs.

--- app/(app)/(tabs-instructor)/_layout.tsx
+++ app/(app)/(tabs-instructor)/_layout.tsx
@@ -1,12 +1,51 @@
+import { primary } from "@/constants/colors";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 
 export default function InstructorTabsLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
-      <Tabs.Screen name="courses" options={{ title: "Courses" }} />
-      <Tabs.Screen name="assignments" options={{ title: "Assignments" }} />
-      <Tabs.Screen name="index" options={{ title: "Dashboard" }} />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: primary,
+      }}
+    >
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="assignments"
+        options={{
+          title: "Assignments",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="clipboard" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="courses"
+        options={{
+          title: "Courses",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="book" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Dashboard",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="grid" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
