69a633adb450dd577ca49f02fde90eb0cec68ed8
ahmedjk34 <ahmedtaher212005@gmail.com>
2026-01-12 19:25:48 +0200

    Trying to figure out how layout work [without doing a condtional render like I'd do in web which works but KIND OF WANT TO LEARN SMTH] work. will be testing something else so keep

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,24 +3,58 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
-import { AuthProvider } from "@/context/AuthContext";
+import { AuthProvider, useAuth } from "@/context/AuthContext";
 import { useColorScheme } from "@/hooks/use-color-scheme";
-import RootGate from "./RootGate";
+import { Stack } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
 
-export default function RootLayout() {
+function RootStack() {
+  const { isAuthenticated, user, isLoading } = useAuth();
   const colorScheme = useColorScheme();
 
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <StatusBar style="auto" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Protected guard={isAuthenticated && user?.role === "admin"}>
+          <Stack.Screen name="(app)/(tabs-admin)" />
+        </Stack.Protected>
+
+        <Stack.Protected guard={isAuthenticated && user?.role === "instructor"}>
+          <Stack.Screen name="(app)/(tabs-instructor)" />
+        </Stack.Protected>
+
+        <Stack.Protected guard={isAuthenticated && user?.role === "student"}>
+          <Stack.Screen name="(app)/(tabs-student)" />
+        </Stack.Protected>
+
+        <Stack.Protected guard={isAuthenticated}>
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        </Stack.Protected>
+
+        <Stack.Protected guard={!isAuthenticated}>
+          <Stack.Screen name="(auth)/index" />
+        </Stack.Protected>
+      </Stack>
+    </ThemeProvider>
+  );
+}
+
+export default function RootLayout() {
   return (
     <AuthProvider>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack screenOptions={{ headerShown: false }} />
-        <RootGate />
-        <StatusBar style="auto" />
-      </ThemeProvider>
+      <RootStack />
     </AuthProvider>
   );
 }
