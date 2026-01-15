d3ee9fab399f88066c1ceb1057001e0669971056
maen thaher <maentcefe@gmail.com>
2026-01-15 07:42:06 +0200

    Analytics Dashboard

--- app/AdminAnalytic.tsx
+++ app/AdminAnalytic.tsx
@@ -1,2 +1,19 @@
+import { Text } from "@react-navigation/elements";
+import { ScrollView, StyleSheet } from "react-native";
+
+export default function AdminAnalyticsScreen(){
+
+return(<ScrollView style={styles.container}>
+    <Text style={styles.title}>Analytics Dashboard</Text>
+    
 
 
+       </ScrollView>)
+
+
+}
+
+const styles = StyleSheet.create({
+    container:{flex:1,backgroundColor:"#F4F6F8",padding:16},
+    title:{fontSize:22,fontWeight:"700",marginBottom:16},
+});
