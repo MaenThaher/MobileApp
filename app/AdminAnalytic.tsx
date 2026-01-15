72d9429e7f0c968e68d22312a5782f55c9bbe630
maen thaher <maentcefe@gmail.com>
2026-01-15 07:47:14 +0200

    add Total Users

--- app/AdminAnalytic.tsx
+++ app/AdminAnalytic.tsx
@@ -1,11 +1,26 @@
+import { Feather } from "@expo/vector-icons";
 import { Text } from "@react-navigation/elements";
-import { ScrollView, StyleSheet } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
 
 export default function AdminAnalyticsScreen(){
+  const [overview, setOverview] = useState<any>(null);
 
+  
 return(<ScrollView style={styles.container}>
     <Text style={styles.title}>Analytics Dashboard</Text>
+    <View style={styles.card}>
     
+        <View style={styles.row}>
+          <Feather name="users" size={20} />
+          <Text style={styles.label}>Total Users</Text>
+        </View>
+        <Text style={styles.value}>{overview.totalUsers}</Text>
+        <Text style={styles.sub}>{overview.activeUsers} active</Text>
+
+
+
+    </View>
 
 
        </ScrollView>)
@@ -16,4 +31,9 @@ return(<ScrollView style={styles.container}>
 const styles = StyleSheet.create({
     container:{flex:1,backgroundColor:"#F4F6F8",padding:16},
     title:{fontSize:22,fontWeight:"700",marginBottom:16},
+    row:{flexDirection:"row",alignItems:"center",gap:8},
+    label:{fontSize:14,color:"#555",},
+    value:{fontSize:14,color:"#555"},
+    sub:{fontSize:12,color:"#888"},
+    card:{backgroundColor:"#FFF",padding:16,borderRadius:12,marginBottom:12},
 });
